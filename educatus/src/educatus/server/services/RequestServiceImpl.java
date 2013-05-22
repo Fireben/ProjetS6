8df29b60adb23d514c554978eb573dbf976026cc
Marc-André Beaudry <Marc-Andre.Beaudry@usherbrooke.ca>
2013-05-22 19:55:02 -0400

    - Ajout de nouvelles requests et reponses dans l'enum. - Ajout de la gestion des exceptions sur le service de requêtes.

--- educatus/src/educatus/server/services/RequestServiceImpl.java
+++ educatus/src/educatus/server/services/RequestServiceImpl.java
@@ -4,6 +4,9 @@ import com.google.gwt.user.server.rpc.RemoteServiceServlet;
 
 import educatus.client.services.RequestService;
 import educatus.shared.request.AbstractRequest;
+import educatus.shared.request.FooterContentRequest;
+import educatus.shared.request.HomePageContentRequest;
+import educatus.shared.request.MainMenuContentRequest;
 import educatus.shared.request.RequestTypeEnum;
 import educatus.shared.response.AbstractResponse;
 import educatus.shared.response.FooterContentResponse;
@@ -22,20 +25,45 @@ public class RequestServiceImpl extends RemoteServiceServlet implements RequestS
 		AbstractResponse response = null;
 		RequestTypeEnum requestType = request.GetRequestType();
 		
-		switch (requestType) {
-			case FOOTER_CONTENT_REQUEST:
-				response = new FooterContentResponse();
-				break;				
-			case HOME_PAGE_CONTENT_REQUEST:
-				response = new HomePageContentResponse();
-				break;				
-			case MAIN_MENU_CONTENT_REQUEST:
-				response = new MainMenuContentResponse();
-				break;	
-			default:
-				break;
+		try {
+			switch (requestType) {
+				case FOOTER_CONTENT_REQUEST:
+					response = ProcessFooterContentRequest((FooterContentRequest) request);
+					break;				
+				case HOME_PAGE_CONTENT_REQUEST:
+					response = ProcessHomePageContentRequest((HomePageContentRequest) request);
+					break;				
+				case MAIN_MENU_CONTENT_REQUEST:
+					response = ProcessMainMenuContentRequest((MainMenuContentRequest) request);
+					break;	
+				default:
+					break;
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 				
 		return response;
 	}
+	
+	private FooterContentResponse ProcessFooterContentRequest(FooterContentRequest request)
+	{
+		FooterContentResponse response = new FooterContentResponse();
+		
+		return response;
+	}
+	
+	private MainMenuContentResponse ProcessMainMenuContentRequest(MainMenuContentRequest request)
+	{
+		MainMenuContentResponse response = new MainMenuContentResponse();
+		
+		return response;
+	}
+	
+	private HomePageContentResponse ProcessHomePageContentRequest(HomePageContentRequest request)
+	{
+		HomePageContentResponse response = new HomePageContentResponse();
+		
+		return response;
+	}	
 }
