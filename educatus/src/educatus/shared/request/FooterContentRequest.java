598e2cbf482c8399ea36c6c31827ab14ee3f41d3
Marc-André Beaudry <Marc-Andre.Beaudry@usherbrooke.ca>
2013-05-22 09:49:53 -0400

    Ajout du serialize UID pour enlever les warnings.

--- educatus/src/educatus/shared/request/FooterContentRequest.java
+++ educatus/src/educatus/shared/request/FooterContentRequest.java
@@ -2,6 +2,7 @@ package educatus.shared.request;
 
 public class FooterContentRequest extends AbstractRequest {
 
+	private static final long serialVersionUID = -7555970829060472658L;
 	private static RequestTypeEnum REQUEST_TYPE = RequestTypeEnum.FOOTER_CONTENT_REQUEST;
 	
 	@Override
