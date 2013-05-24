3d2d75bdac0aec063deda6e0772e6744e6cff5f0
P-A Fortier <snow_paf@hotmail.com>
2013-05-24 11:09:39 -0400

    SeminaryViewPresenter clean

--- educatus/src/educatus/client/view/SeminaryViewView.java
+++ educatus/src/educatus/client/view/SeminaryViewView.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package educatus.client.view;
 
 import com.gwtplatform.mvp.client.ViewImpl;
@@ -7,12 +6,8 @@ import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.dom.client.Element;
 import com.google.gwt.user.client.ui.FlowPanel;
-import com.google.gwt.user.client.ui.Image;
-import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.Widget;
-import com.google.inject.Inject;
 
-import educatus.client.presenter.SeminaryViewPresenter;
 import educatus.client.presenter.SeminaryViewPresenter.MyView;
 
 public class SeminaryViewView extends ViewImpl implements MyView {
@@ -86,73 +81,3 @@ public class SeminaryViewView extends ViewImpl implements MyView {
 	  }
 	 
 }
-=======
-package educatus.client.view;
-
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.user.client.ui.Widget;
-import com.gwtplatform.mvp.client.ViewImpl;
-
-import educatus.client.presenter.SeminaryViewPresenter.MyView;
-
-public class SeminaryViewView extends ViewImpl implements MyView {
-	  
-	interface SeminaryViewViewUiBinder extends UiBinder<Widget, SeminaryViewView> {
-	  }
-
-	  private static SeminaryViewViewUiBinder uiBinder = GWT.create(SeminaryViewViewUiBinder.class);
-
-	  private final Widget widget;
-	  
-	  //@UiField Label seminaryTitle;
-	  //@UiField Label writtenBy;
-	  //@UiField Label modifyBy;
-	  //@UiField Label author;
-	  //@UiField Label modif;
-	  //@UiField Label createdDate;
-	  //@UiField Label modifiedDate;
-	  //@UiField Label seminaryContent;
-
-
-	//public Label getSeminaryTitle() {
-	//	return seminaryTitle;
-	//}
-
-	//public Label getWrittenBy() {
-	//	return writtenBy;
-	//}
-
-	/*public Label getModifyBy() {
-		return modifyBy;
-	}
-
-	public Label getAuthor() {
-		return author;
-	}
-
-	public Label getModif() {
-		return modif;
-	}
-
-	public Label getCreatedDate() {
-		return createdDate;
-	}
-
-	public Label getModifiedDate() {
-		return modifiedDate;
-	}
-
-	public Label getSeminaryContent() {
-		return seminaryContent;
-	}*/
-
-	public SeminaryViewView() {
-	    widget = uiBinder.createAndBindUi(this);
-	  }
-
-	  public Widget asWidget() {
-	    return widget;
-	  }
-}
->>>>>>> remotes/origin/Version_0_1
