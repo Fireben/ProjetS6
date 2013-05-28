cdf397b50dae299a6a947c3cef1200de53fb0cb9
Marc-André Beaudry <Marc-Andre.Beaudry@usherbrooke.ca>
2013-05-28 15:15:18 -0400

    Ajout du dossier war/educatus dans le .gitignore

--- educatus/src/educatus/shared/services/requestservice/AbstractRequest.java
+++ educatus/src/educatus/shared/services/requestservice/AbstractRequest.java
@@ -4,5 +4,6 @@ import java.io.Serializable;
 
 public abstract class AbstractRequest implements Serializable {
 	private static final long serialVersionUID = 1L;
+
 	public abstract RequestTypeEnum GetRequestType();
 }
--- educatus/src/educatus/shared/services/requestservice/AbstractResponse.java
+++ educatus/src/educatus/shared/services/requestservice/AbstractResponse.java
@@ -2,9 +2,9 @@ package educatus.shared.services.requestservice;
 
 import java.io.Serializable;
 
-
 public abstract class AbstractResponse implements Serializable {
 	private static final long serialVersionUID = 1L;
+
 	public abstract ResponseTypeEnum GetResponseType();
 
 }
