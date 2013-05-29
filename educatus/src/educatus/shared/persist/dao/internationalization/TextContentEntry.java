3a2e042ff0c31b84a137a475e67012c3898eb28d
Marc-André Beaudry <Marc-Andre.Beaudry@usherbrooke.ca>
2013-05-29 00:20:55 -0400

    Correction d'une erreur dans les namedQueries de TextContentEntry et TextContentTranslationEntry.

--- educatus/src/educatus/shared/persist/dao/internationalization/TextContentEntry.java
+++ educatus/src/educatus/shared/persist/dao/internationalization/TextContentEntry.java
@@ -8,11 +8,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
+@NamedQueries({
+	@NamedQuery(name = TextContentEntry.FIND_ALL, query = "SELECT t FROM TextContentEntry t")})
 @Table(name = "internationalization.textcontententry")
 public class TextContentEntry implements Serializable {
 	private static final long serialVersionUID = 1L;
